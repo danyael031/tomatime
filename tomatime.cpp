c4ea721f298de9657c6c8e9a0143a18e10eac52f
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2019-01-20 23:39:39 -0700

    resubido porque creo que fallé con el add de git

--- tomatime.cpp
+++ tomatime.cpp
@@ -3,13 +3,34 @@
  */
 
 #include <iostream>
+#include <ctime>
+#include <cmath>
 
-int main(){
-	std::cout<<"Hola mundo!"<<std::endl;
-	std::cout<<"Primer proyecto subido a github"<<std::endl;
+using std::cout;
+using std::endl;
+using std::cin;
+
+int tomatime(){
 	return 0;
+}
 
+int main(int argc, char *argv[]){
+	clock_t iniTiempo = clock();
+	for (int i = 0; i<=1000000; i++){
+		cout<<i<<endl;
+	}
+	
+	double segundos = (clock() - iniTiempo) / (double) CLOCKS_PER_SEC;
+	cout << segundos << endl;	
+}
 
-
+int procArg(int numArg, char args[]){
+	
+	for (int i = 0; i < numArg; i++){
+		
+		cout<<args[i]<<endl;
+	}
+	cout<<"Primer proyecto subido a github"<<endl;
+	return 0;
 
 }
